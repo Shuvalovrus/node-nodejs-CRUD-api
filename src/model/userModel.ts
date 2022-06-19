40c3fa7fa47ab323c7b07f39cc321e91c98ee4ea
Konstantin <shuvalov.2021@list.ru>
2022-06-19 14:58:56 +0300

    feat: add model

---
+++ src/model/userModel.ts
@@ -0,0 +1,35 @@
+import * as users from "../db/database.json";
+import { v4 } from "uuid";
+import { writeFile } from 'fs';
+
+
+const usersArray = JSON.parse(JSON.stringify(users)).default;
+
+interface IUser { 
+    id:string,
+    username: string,
+    age: number,
+    hobbies: string[] | []
+}
+
+export const findAllUsers = ():Promise<Array<IUser>> => {
+    return new Promise((resolve : any, reject) => {
+        resolve(usersArray)
+    })
+}
+
+export const findUser = (id: string):Promise<IUser> => {
+    return new Promise((resolve : any, reject) => {
+        resolve(usersArray.find((user: IUser) => user.id === id))
+    })
+}
+
+export const create = (user:any) => {
+    return new Promise((resolve, reject) => {
+        const newUser = {id: v4(), ...user};
+        usersArray.push(newUser);
+
+        writeFile("./src/db/database.json", JSON.stringify(usersArray), 'utf-8', (err) => console.log(err));
+        resolve(newUser);
+    })
+}
