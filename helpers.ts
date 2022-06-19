a495bfd6776b9cfa7fd421baf6213feb8133a0c4
Konstantin <shuvalov.2021@list.ru>
2022-06-19 16:43:39 +0300

    feat: add help functions

---
+++ helpers.ts
@@ -0,0 +1,32 @@
+import { validate } from 'uuid';
+
+export const validateId = (req: any, res: any, callback: any) => {
+    const id = req.url?.split('/').pop() || '';
+
+    if (validate(id)) {
+        callback(req, res, id);
+
+    } else { 
+        res.writeHead(400, 'Invalid User Id', {'content-type' : 'text/plain'});
+        res.end('Invalid User Id');
+    }
+
+} 
+
+export const getBodyData = (req:any, res:any) => {
+    return new Promise( (resolve, reject) => {
+        try {
+            let body = '';
+
+            req.on('data', (chunk: string) => {
+                body += chunk.toString();
+            })
+
+            req.on('end', async () => {
+                resolve(body);
+            })
+        } catch (err) {
+            reject(err);
+        }        
+    })
+}
